31ba33ef9b0eb235b376e4549d7fba332426acc0
uarabacioglu <uarabacioglu@gmail.com>
2025-11-20 17:28:35 +0300

    robots.txt ve sitemap ayarları yapıldı

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import netlify from "@astrojs/netlify";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
+
 export default defineConfig({
+  site: "https://sidejeepsafari.com",
   output: 'server',
 
   vite: {
     plugins: [tailwindcss()]
   },
+
+  integrations: [sitemap()],
 
   i18n: {
     locales: ["de", "en", "tr", "ru"],
