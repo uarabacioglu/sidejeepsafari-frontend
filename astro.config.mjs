35ef10947581f6f540942932f7831f4fd7babe11
uarabacioglu <uarabacioglu@gmail.com>
2025-11-20 12:35:56 +0300

    Add Netlify adapter for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   vite: {
     plugins: [tailwindcss()]
   },
+
   i18n: {
     locales: ["de", "en", "tr", "ru"],
     defaultLocale: "en",
-  }
+  },
+
+  adapter: netlify()
 });
